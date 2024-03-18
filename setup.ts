c7aae88db326d8ef72e66af433e47051d4338a9d
adam krawiec <a.krawiec@exlabs.co.uk>
2024-03-18 10:57:48 +0100

    starting ts adventure, simple train search engine

---
+++ setup.ts
@@ -0,0 +1,28 @@
+// dummy file with some vars setup for repl testing
+import { Train, Route, Station, SearchEngine } from './src/example'
+
+const katowice = new Station('Katowice');
+const gliwice = new Station('Gliwice');
+const warsaw = new Station('Warsaw');
+const gdansk = new Station('Gdansk');
+
+const katowiceToWarsaw = new Route(katowice, new Date(1970,1,1,8,0), warsaw, new Date(1970,1,1,11,0))
+const GliwiceToGdansk = new Route(gliwice, new Date(1970,1,1,8,0), gdansk, new Date(1970,1,1,11,0))
+const katowiceToGliwice = new Route(katowice, new Date(1970,1,1,9,0), gliwice, new Date(1970,1,1,9,30))
+
+const katowiceToWarsawTrain = new Train('TLK', katowiceToWarsaw)
+const gliwiceToGdankTrain = new Train('IC', GliwiceToGdansk)
+const katowiceToGliwiceTrain = new Train('S1', katowiceToGliwice)
+
+const searchEngine = new SearchEngine();
+
+export { 
+  katowiceToWarsawTrain,
+  gliwiceToGdankTrain,
+  katowiceToGliwiceTrain,
+  searchEngine,
+  Train,
+  Route,
+  Station,
+  SearchEngine
+}
